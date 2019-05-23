cd847589f7ea1576d1d3a262c2299ca7a54a8215
Jens Fischer <jensfischer95@gmail.com>
2019-05-24 00:48:47 +0200

    Start customizing package.json

--- src/Main.hx
+++ src/Main.hx
@@ -2,9 +2,5 @@ import vscode.*;
 
 class Main {
 	@:expose("activate")
-	static function activate(context:ExtensionContext) {
-		context.subscriptions.push(Vscode.commands.registerCommand("hellohaxe.sayHello", function() {
-			Vscode.window.showInformationMessage("Hello from Haxe!");
-		}));
-	}
+	static function activate(context:ExtensionContext) {}
 }
