fe32682dda76a5e527fb69127191ab73e878b178
Jens Fischer <jensfischer95@gmail.com>
2019-05-30 23:38:33 +0200

    Looks like officialOnline() works, officialInstalled() doesn't?

--- src/Extension.hx
+++ src/Extension.hx
@@ -1,10 +1,35 @@
 import vscode.*;
 import Vscode.*;
+import lix.client.haxe.Switcher;
+import haxeshim.Scope;
+
+using Lambda;
 
 class Extension {
 	@:expose("activate")
 	static function activate(context:ExtensionContext) {
-		var cwd = workspace.workspaceFolders[0].uri.fsPath;
-		trace(haxeshim.Scope.seek({cwd: cwd}).haxeInstallation.compiler);
+		var folders = workspace.workspaceFolders;
+		if (folders.length == 0) {
+			return;
+		}
+		var cwd = folders[0].uri.fsPath;
+		var scope = Scope.seek({cwd: cwd});
+		var switcher = new Switcher(scope, true, _ -> {});
+		switcher.officialInstalled(IncludePrereleases).map(o -> {
+			switch o {
+				case Success(data):
+					trace(data.array());
+				case Failure(failure):
+					trace(failure);
+			}
+		});
+		Switcher.officialOnline(IncludePrereleases).map(o -> {
+			switch o {
+				case Success(data):
+					trace(data.array());
+				case Failure(failure):
+					trace(failure);
+			}
+		});
 	}
 }
