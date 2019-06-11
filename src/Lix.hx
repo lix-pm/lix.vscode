4ab5b96d3fe45c478147d09ed2feba6276a71cc1
Jens Fischer <jensfischer95@gmail.com>
2019-06-11 15:21:31 +0200

    Add a lix output channel

--- src/Lix.hx
+++ src/Lix.hx
@@ -1,8 +1,11 @@
+import haxeshim.Logger;
+
 class Lix {
 	public var scope(default, null):Scope;
 	public var switcher(default, null):Switcher;
 	public var onDidChangeScope(get, never):Event<Void>;
 
+	final outputChannel:OutputChannel;
 	final folder:WorkspaceFolder;
 	final _onDidChangeScope = new EventEmitter<Void>();
 
@@ -11,6 +14,8 @@ class Lix {
 
 	public function new(context, folder) {
 		this.folder = folder;
+
+		outputChannel = window.createOutputChannel("lix");
 
 		var watcher = workspace.createFileSystemWatcher(new RelativePattern(folder, ".haxerc"));
 		watcher.onDidChange(_ -> update());
@@ -23,9 +28,45 @@ class Lix {
 
 	function update() {
 		scope = Scope.seek({cwd: folder.uri.fsPath});
-		switcher = new Switcher(scope, true, _ -> {});
+		@:privateAccess scope.logger = new OutputChannelLogger(outputChannel);
+		switcher = new Switcher(scope, true, outputChannel.appendLine);
+
+		if (scope.isGlobal) {
+			outputChannel.hide();
+		} else {
+			outputChannel.show();
+		}
 
 		// TOOD: check if there were actually any changes?
 		_onDidChangeScope.fire();
+	}
+}
+
+private class OutputChannelLogger extends Logger {
+	final outputChannel:OutputChannel;
+
+	public function new(outputChannel:OutputChannel) {
+		super();
+		this.outputChannel = outputChannel;
+	}
+
+	override function error(s:String) {
+		outputChannel.appendLine('[error] $s');
+	}
+
+	override function info(s:String) {
+		outputChannel.appendLine('[info] $s');
+	}
+
+	override function warning(s:String) {
+		outputChannel.appendLine('[warning] $s');
+	}
+
+	override function progress(s:String) {
+		outputChannel.appendLine('[progress] $s');
+	}
+
+	override function success(s:String) {
+		outputChannel.appendLine('[success] $s');
 	}
 }
