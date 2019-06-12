bc1bb1dfd675417bc29d88f754106c055d5a3bc2
Jens Fischer <jensfischer95@gmail.com>
2019-06-12 17:00:39 +0200

    Revert "Add a "Select Haxe Version" buttons to Haxe Dependencies + .haxerc files"
    This reverts commit cd6824335399a71274c5eba2f01d163b4c256013.

--- src/LixCommand.hx
+++ src/LixCommand.hx
@@ -1,6 +1,7 @@
 enum abstract LixCommand(String) to String {
+	var SelectHaxeVersion = "lix.selectHaxeVersion";
+	var CreateScope = "lix.createScope";
 	var InitializeProject = "lix.initializeProject";
 	var DownloadMissingDependencies = "lix.downloadMissingDependencies";
 	var InstallLibrary = "lix.installLibrary";
-	var SelectHaxeVersion = "lix.selectHaxeVersion";
 }
