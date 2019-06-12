1b10e96a634389679d52967ab3c7813894caf1b5
Jens Fischer <jensfischer95@gmail.com>
2019-06-12 14:21:49 +0200

    Extract the CLI logic into a separate method

--- src/Haxelib.hx
+++ src/Haxelib.hx
@@ -12,23 +12,28 @@ class Haxelib {
 		this.installation = installation;
 	}
 
-	public function getLibraries():Null<Array<String>> {
-		if (libraries != null) {
-			return libraries;
-		}
+	function run(command:String):Null<Array<String>> {
 		var haxelib = installation.installation.haxelibExecutable;
 		if (haxelib == null) {
 			return null;
 		}
 		try {
-			var result:Buffer = ChildProcess.execSync('$haxelib search ""', {cwd: folder.uri.fsPath});
-			var libraries = result.toString().split("\n").map(StringTools.trim);
-			libraries.pop(); // empty line
-			libraries.pop(); // "n libraries found"
-			libraries.sort((a, b) -> Reflect.compare(a.toLowerCase(), b.toLowerCase()));
-			return this.libraries = libraries;
+			var result:Buffer = ChildProcess.execSync('$haxelib $command', {cwd: folder.uri.fsPath});
+			var output = result.toString().split("\n").map(StringTools.trim);
+			output.pop(); // empty line
+			return output;
 		} catch (_:Any) {
 			return null;
 		}
+	}
+
+	public function getLibraries():Null<Array<String>> {
+		if (libraries != null) {
+			return libraries;
+		}
+		var libraries = run('search ""');
+		libraries.pop(); // "n libraries found"
+		libraries.sort((a, b) -> Reflect.compare(a.toLowerCase(), b.toLowerCase()));
+		return this.libraries = libraries;
 	}
 }
